6522d1d737b9ca2be1186c1bd24de85d7b593a04
mgibbs189 <mgibbs189@gmail.com>
2018-12-05 14:10:23 -0500

    Rename `wp.hooks` to `FWP.hooks` to support WP 5.0

--- assets/js/front.js
+++ assets/js/front.js
@@ -1,5 +1,5 @@
 (function($) {
-    wp.hooks.addAction('facetwp/refresh/hierarchy_select', function($this, facet_name) {
+    FWP.hooks.addAction('facetwp/refresh/hierarchy_select', function($this, facet_name) {
         var selected_values = [];
         $this.find('.facetwp-hierarchy_select option:selected').each(function() {
             var value = $(this).attr('value');
@@ -10,7 +10,7 @@
         FWP.facets[facet_name] = selected_values;
     });
 
-    wp.hooks.addFilter('facetwp/selections/hierarchy_select', function(output, params) {
+    FWP.hooks.addFilter('facetwp/selections/hierarchy_select', function(output, params) {
         var selected_values = [];
         params.el.find('.facetwp-hierarchy_select option:selected').each(function(i) {
             var value = $(this).attr('value');
